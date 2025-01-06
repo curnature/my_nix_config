2a8bec672f75672af058baf2cfb7458698bc77ab
curnature <curnature00@gmail.com>
2025-01-06 11:55:05 -0500

    modify zsh settings in HM

--- modules/home-manager/zsh/default.nix
+++ modules/home-manager/zsh/default.nix
@@ -1,7 +1,24 @@
-
+{ pkgs, ... }:
 {
     programs.zsh = {
+        enable = true;
         initExtra = ''
+            # Lines configured by zsh-newuser-install
+            HISTFILE=~/.histfile
+            HISTSIZE=1000
+            SAVEHIST=1000
+            setopt autocd extendedglob nomatch notify
+            unsetopt beep
+            bindkey -v
+            # End of lines configured by zsh-newuser-install
+            # The following lines were added by compinstall
+            zstyle :compinstall filename '/home/curvature/.zshrc'
+
+            autoload -Uz compinit
+            compinit
+            # End of lines added by compinstall
+            
+            # Alias settings
             alias ksshlongleaf="kitty +kitten ssh -X tnwang@longleaf.unc.edu"
         '';
     };
